72474e02cb018cc956fdf4adb64408b6144df9a9
Benicio Lopez <benicio.lopez@gmail.com>
2022-06-22 13:10:04 -0500

    program now calculates mortgage accurately, just need to format to currency

--- src/mortgageCalculator/Main.java
+++ src/mortgageCalculator/Main.java
@@ -5,9 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
-        System.out.println("The principal is " + principal);
         System.out.print("Annual Interest Rate: ");
-        float interestRate = scanner.nextFloat();
-        System.out.println("Here dat interest rate: " + interestRate);
+        float annualInterestRate = scanner.nextFloat();
+        System.out.print("Period (Years): ");
+        int period = scanner.nextInt();
+        float monthlyInterestRate = annualInterestRate / 100 / 12;
+        int payments = period * 12;
+        double mortgage = principal * ((monthlyInterestRate * Math.pow((1 + monthlyInterestRate), payments)) / (Math.pow((1 + monthlyInterestRate), payments) - 1));
+        System.out.print("Mortgage: " + mortgage);
     }
 }
