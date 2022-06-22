2b08bd46964459ee5c6c0d5f3a4c152f034c97cf
Benicio Lopez <benicio.lopez@gmail.com>
2022-06-22 13:16:47 -0500

    displays mortgage in currency format

--- src/mortgageCalculator/Main.java
+++ src/mortgageCalculator/Main.java
@@ -1,4 +1,5 @@
 package mortgageCalculator;
+import java.text.NumberFormat;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,8 @@ public class Main {
         float monthlyInterestRate = annualInterestRate / 100 / 12;
         int payments = period * 12;
         double mortgage = principal * ((monthlyInterestRate * Math.pow((1 + monthlyInterestRate), payments)) / (Math.pow((1 + monthlyInterestRate), payments) - 1));
-        System.out.print("Mortgage: " + mortgage);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(mortgage);
+        System.out.print("Mortgage: " + result);
     }
 }
